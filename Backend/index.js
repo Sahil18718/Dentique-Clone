828c3e04d852b7556c732a0e2ed232d7a7c216f8
mmrhra851 <mmehra851@gmail.com>
2023-06-15 22:16:42 +0530

    day2

--- Backend/index.js
+++ Backend/index.js
@@ -6,8 +6,15 @@ const registerRouter = require('./Routes/register.routes');
 const { connection } = require('./configs/db');
 const { appointmentRouter } = require('./Routes/appointment');
 require('dotenv').config();
-
 const app = express();
+
+const http = require("http");
+const {Server} = require("socket.io");
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer);
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -26,9 +33,18 @@ app.get("/",(req,res)=>{
     res.send("Dentique Backend Home Page")
 })
 
+app.get("/chat",(req,res)=>{
+    res.sendFile(__dirname + "/Fronten/chat/chat.html")
+})
 
-app.listen(process.env.PORT, ()=>{
+io.on("connection",(socket)=>{
+    socket.on("chat1",(msg)=>{
+        io.emit("chat2",msg)
+    })
+})
+
+
+httpServer.listen(process.env.PORT, ()=>{
     connection();
     console.log(`Server is running @ http://localhost:${process.env.PORT}`)
 })
-
