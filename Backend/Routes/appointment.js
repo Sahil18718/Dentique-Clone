882418d2cd55ca74ec67edd427c99da386038249
Anandhu_P_A <anandhupa131@gmail.com>
2023-06-15 19:21:55 +0530

    Appoint model added , ui creation started for doctor listing page

---
+++ Backend/Routes/appointment.js
@@ -0,0 +1,35 @@
+const { AppointmentModel } = require("../Models/appointment.model");
+
+const appointmentRouter = require("express").Router();
+
+
+appointmentRouter.post("/",async(req,res)=>{
+    try{
+    //___________setting dummy data
+    req.body.userId="1"
+    req.body.doctorId="1"
+    //___________dummy data set
+    let {userId,doctorId,message,}=req.body;
+    if(!userId){res.status(401).json("you are not authorized")}
+    else if (!doctorId){res.status(507).json('No doctor selected')}
+    else {
+        let newAppointment = new AppointmentModel(req.body);
+        let output= await newAppointment.save();
+        res.send(output)
+    }
+    }catch(err){
+        console.log("error in appointmentRouter | get",error)
+        res.status(500).json("server error")
+}
+})
+
+
+
+
+
+
+
+
+
+
+module.exports ={appointmentRouter}
