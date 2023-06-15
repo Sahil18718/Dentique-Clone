743510b99b40d107c9b0171b99340aeec8fe3e26
Mangalam Kumar <mangalamkumar2002@gmail.com>
2023-06-15 19:43:02 +0530

    Frontend/Backend

--- Backend/Routes/login.routes.js
+++ Backend/Routes/login.routes.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', async (req, res)=>{
+loginRouter.post('/patient', async (req, res)=>{
     try{
         let data = req.body;
         const userDetails = await userModel.find({email: data.email});
@@ -14,7 +14,7 @@ loginRouter.post('/', async (req, res)=>{
             return res.status(400).send({msg: "User Doesn't Exist, create an account"});
         }
         let result = await bcrypt.compare(data.password, userDetails[0].password);
-        if(result){
+        if(result && userDetails[0].role==='Patient'){
             let accessToken = jwt.sign({userId: userDetails._id}, process.env.JWT_SECRET_KEY, {expiresIn: '4h'});
             res.cookie('token', accessToken, {maxAge: 60*4})
             res.status(200).send({msg: 'Login Successful'});
@@ -27,22 +27,47 @@ loginRouter.post('/', async (req, res)=>{
     }
 })
 
-// loginRouter.post('/admin', (req, res)=>{
-//     try{
-    
-//     }catch(err){
-//         console.log(err.message);
-//         res.status(500).send({msg: err.message});
-//     }
-// })
 
-// loginRouter.post('/doctor', (req, res)=>{
-//     try{
-    
-//     }catch(err){
-//         console.log(err.message);
-//         res.status(500).send({msg: err.message});
-//     }
-// })
+loginRouter.post('/admin', async (req, res)=>{
+    try{
+        let data = req.body;
+        const userDetails = await userModel.find({email: data.email});
+        if(userDetails.length===0){
+            return res.status(400).send({msg: "User Doesn't Exist, create an account"});
+        }
+        let result = await bcrypt.compare(data.password, userDetails[0].password);
+        if(result && userDetails[0].role==='Admin'){
+            let accessToken = jwt.sign({userId: userDetails._id}, process.env.JWT_SECRET_KEY, {expiresIn: '4h'});
+            res.cookie('token', accessToken, {maxAge: 60*4})
+            res.status(200).send({msg: 'Login Successful'});
+        }else{
+            res.status(400).send({msg: 'Wrong Credentials'});
+        }
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send({msg: err.message});
+    }
+})
+
+loginRouter.post('/doctor', async (req, res)=>{
+    try{
+        let data = req.body;
+        const userDetails = await userModel.find({email: data.email});
+        if(userDetails.length===0){
+            return res.status(400).send({msg: "User Doesn't Exist, create an account"});
+        }
+        let result = await bcrypt.compare(data.password, userDetails[0].password);
+        if(result && userDetails[0].role==='Doctor'){
+            let accessToken = jwt.sign({userId: userDetails._id}, process.env.JWT_SECRET_KEY, {expiresIn: '4h'});
+            res.cookie('token', accessToken, {maxAge: 60*4})
+            res.status(200).send({msg: 'Login Successful'});
+        }else{
+            res.status(400).send({msg: 'Wrong Credentials'});
+        }
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send({msg: err.message});
+    }
+})
 
 module.exports = loginRouter;
---
+++ Frontend/admin/js/adminLogin.js
@@ -0,0 +1,23 @@
+const baseServerUrl = 'http://localhost:8998/login/admin';
+const emailEl = document.querySelector('input[type="email"]');
+const passwordEl = document.querySelector('input[type="password"]');
+const formEl = document.querySelector('#container .loginSection form');
+formEl.addEventListener('submit', (evnt)=>{
+    evnt.preventDefault();
+    let email = emailEl.value;
+    let password = passwordEl.value;
+    login(email, password);
+})
+
+const login = async (email, password) => {
+    let result = await fetch(`${baseServerUrl}`,{
+        body: JSON.stringify({...{email}, ...{password}}),
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    });
+    if(result.ok){
+        alert('Login Successful');
+    }
+}
