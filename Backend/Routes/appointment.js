5bfab62fb136f8d4649b8635a53bc10635109d05
Anandhu_P_A <anandhupa131@gmail.com>
2023-06-16 17:09:34 +0530

    Appointment booking page ui created, and functionality added

--- Backend/Routes/appointment.js
+++ Backend/Routes/appointment.js
@@ -25,19 +25,25 @@ appointmentRouter.get("/",async(req,res)=>{
 appointmentRouter.post("/",async(req,res)=>{
     try{
     //___________setting dummy data
-    req.body.userId="1"
-    req.body.doctorId="1"
+    req.body.userId="648c47af54f8af600e3e1d45"
+   
     //___________dummy data set
-    let {userId,doctorId,message,}=req.body;
+    let {userId,doctorId,message,time,date}=req.body;
     if(!userId){res.status(401).json("you are not authorized")}
     else if (!doctorId){res.status(507).json('No doctor selected')}
+    else if(!time){res.status(507).json('No time selected')}
+    else if (!date){res.status(507).json("No date selected")}
     else {
+        let appointmentExists = await AppointmentModel.findOne({time,date,doctorId})
+        if(appointmentExists){res.status(409).json("That slot is already booked")}
+        else{
         let newAppointment = new AppointmentModel(req.body);
         let output= await newAppointment.save();
         res.send(output)
+            }
     }
     }catch(err){
-        console.log("error in appointmentRouter | get",error)
+        console.log("error in appointmentRouter | get",err)
         res.status(500).json("server error")
 }
 })
