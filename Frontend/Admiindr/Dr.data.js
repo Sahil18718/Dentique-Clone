dc2c3e8c8e4059b5c7dfd579f6f933112ea0cf4b
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2023-06-19 10:45:09 +0530

    .

--- Frontend/Admiindr/Dr.data.js
+++ Frontend/Admiindr/Dr.data.js
@@ -1,34 +1,34 @@
 
 
-$(document).ready(function() {
-  $('form').submit(function(event) {
-    event.preventDefault(); // Prevent form submission
+// $(document).ready(function() {
+//   $('form').submit(function(event) {
+//     event.preventDefault(); // Prevent form submission
 
-    // Collect form data
-    var formData = {
-      name: $('#name').val(),
-      age: $('#age').val(),
-      email: $('#email').val(),
-      imageURL: $('#imageURL').val(),
-      feesAddress: $('#feesAddress').val(),
-      description: $('#description').val()
-    };
+//     // Collect form data
+//     var formData = {
+//       name: $('#name').val(),
+//       age: $('#age').val(),
+//       email: $('#email').val(),
+//       imageURL: $('#imageURL').val(),
+//       feesAddress: $('#feesAddress').val(),
+//       description: $('#description').val()
+//     };
 
-    // Send AJAX request
-    $.ajax({
-      type: 'POST',
-      url: 'https://greasy-sofa-244-production.up.railway.app/doctor/newdr',
-      data: formData,
-      success: function(response) {
-        // Handle success response
-        console.log('Form data submitted successfully!');
-        console.log(response); // Log the response from the server
-      },
-      error: function(error) {
-        // Handle error response
-        console.error('Error submitting form data:');
-        console.error(error);
-      }
-    });
-  });
-});
+//     // Send AJAX request
+//     $.ajax({
+//       type: 'POST',
+//       url: 'https://greasy-sofa-244-production.up.railway.app/doctor/newdr',
+//       data: formData,
+//       success: function(response) {
+//         // Handle success response
+//         console.log('Form data submitted successfully!');
+//         console.log(response); // Log the response from the server
+//       },
+//       error: function(error) {
+//         // Handle error response
+//         console.error('Error submitting form data:');
+//         console.error(error);
+//       }
+//     });
+//   });
+// });
